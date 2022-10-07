20f5ea91d946dcb2f43f6d8e99225c50f2f3a251
angie10 <delacruzvargasangelica26@gmail.com>
2022-10-07 09:40:51 -0400

    almost finished

---
+++ index.js
@@ -0,0 +1,8 @@
+const bioAlicent = document.getElementById('#bio1')
+const alicent = document.querySelector('.alicent')
+
+
+// bioAlicent.addEventListener('click', () => alicent.classList.add('.hidden'))
+
+        
+// bioAlicent.addEventListener('click', () => alicent.classList.remove('.hidden'))
